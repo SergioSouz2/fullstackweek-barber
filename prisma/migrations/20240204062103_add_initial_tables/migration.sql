a6a5ef9723aa543acea8bbada87d91b04988716a
Sergio Souz2 <sergio.tbl0123@gmail.com>
2024-02-04 03:28:13 -0300

    chore: add seed script, ajuste no schema

---
+++ prisma/migrations/20240204062103_add_initial_tables/migration.sql
@@ -0,0 +1,38 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `barbershopId` on the `Booking` table. All the data in the column will be lost.
+  - You are about to drop the `BarberShop` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `babershopId` to the `Booking` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `status` to the `Booking` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Booking" DROP CONSTRAINT "Booking_barbershopId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Service" DROP CONSTRAINT "Service_barbershopId_fkey";
+
+-- AlterTable
+ALTER TABLE "Booking" DROP COLUMN "barbershopId",
+ADD COLUMN     "babershopId" TEXT NOT NULL,
+ADD COLUMN     "status" TEXT NOT NULL;
+
+-- DropTable
+DROP TABLE "BarberShop";
+
+-- CreateTable
+CREATE TABLE "Barbershop" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+
+    CONSTRAINT "Barbershop_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Service" ADD CONSTRAINT "Service_barbershopId_fkey" FOREIGN KEY ("barbershopId") REFERENCES "Barbershop"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Booking" ADD CONSTRAINT "Booking_babershopId_fkey" FOREIGN KEY ("babershopId") REFERENCES "Barbershop"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
